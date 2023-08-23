11da18df7ce1fe8a4f8d2b1255b9d59ea565af8b
Glauber Ferreira <glauber.ferreira@ifal.edu.br>
2023-08-23 14:07:51 -0300

    adiciona um TextInput nome no componente Gato

--- App.js
+++ App.js
@@ -1,19 +1,32 @@
-import { Text } from 'react-native';
+import { useState } from 'react';
+import { Text, TextInput } from 'react-native';
 
 const MyApp = () => {
   return (
     <>
       <Text>Hello World</Text>
       <Text>IFAL</Text>
-      <Gato nome="Jubileu" idade={1}/>
-      <Gato nome="Garfield" idade={3}/>
-      <Gato nome="Marie" idade={12}/>
+      <Gato idade={1}/>
+      <Gato idade={3}/>
+      <Gato idade={12}/>
     </>
   )
 }
 
-const Gato = ({nome, idade}) => {
-  return <Text>Eu sou um(a) gato(a), meu nome é {nome.toUpperCase()} e eu tenho {idade} {idade === 1 ? "ano" : "anos"}.</Text>
+const Gato = ({idade}) => {
+  const [nome, setNome] = useState('');
+
+  return (
+    <>
+      <TextInput 
+        onChangeText={(texto) => setNome(texto)}
+        defaultValue={nome}
+        placeholder='Informe o nome do gato...'
+        style={{borderWidth: 1, height: 40}}
+      />
+      <Text>Eu sou um(a) gato(a), meu nome é {nome.toUpperCase()} e eu tenho {idade} {idade === 1 ? "ano" : "anos"}.</Text>
+    </>
+  );
 }
 
 export default MyApp;
